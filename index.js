bd63a04015977d2167ae8fd161c54de387a7ce11
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-09-24 20:44:07 -0300

    body-parser

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 
 const dbConfig = require('./config/database');
 
+// Connection
 // mongoose.connect('mongodb://user:password@localhost/twitter');
 mongoose.connect(dbConfig.url);
 
+// autoLoad
 requireDir(dbConfig.modelsPath);
 
-const User = mongoose.model('User');
-User.create({
-  name: 'Jesus',
-  username: 'Cristo',
-  email: 'jesus@jesus.com',
-  password: '132',
+// Config app
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+
+  await User.create(req.body);
+
+  return res.send('OK');
 });
 
 // console.log(User);
