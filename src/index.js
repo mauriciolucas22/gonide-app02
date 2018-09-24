0443b820191c7fe40524493acdfe57e74135f1bd
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-09-24 20:01:26 -0300

    connected

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const app = require('express')();
+const mongoose = require('mongoose');
+
+const dbConfig = require('../config/database');
+
+// mongoose.connect('mongodb://user:password@localhost/twitter');
+mongoose.connect(dbConfig.url);
 
 app.get('/', (req, res) => {
   res.send('Jesus loves you!');
