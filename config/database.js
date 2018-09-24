9af469c9e64fd0f8475942ddcc1e38c6fe2a4c8d
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-09-24 20:24:26 -0300

    mongo conectado e cadastrando

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
+const path = require('path');
+
 module.exports = {
   url: 'mongodb://localhost/twitter',
+
+  // path: de onde o index.js é executado
+  modelsPath: path.resolve('app', 'models'),
 };
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const dbConfig = require('./config/database');
 
 // mongoose.connect('mongodb://user:password@localhost/twitter');
 mongoose.connect(dbConfig.url);
 
-app.get('/', (req, res) => {
-  res.send('Jesus loves you!');
-  res.end();
+requireDir(dbConfig.modelsPath);
+
+const User = mongoose.model('User');
+User.create({
+  name: 'Jesus',
+  username: 'Cristo',
+  email: 'jesus@jesus.com',
+  password: '132',
 });
+
+// console.log(User);
 
 app.listen(3000);
