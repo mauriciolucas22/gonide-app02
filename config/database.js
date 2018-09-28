43805fb9adcecf7aeae20942273580f8370b5beb
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-09-27 22:27:40 -0300

    dotenv

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  url: 'mongodb://localhost/twitter',
+  url: process.env.DATABASE_URL,
 
   // path: de onde o index.js é executado
   modelsPath: path.resolve('app', 'models'),
--- config/sentry.js
+++ config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://10f9d93276e94d4cb04d2e973254471a@sentry.io/1289296',
+  dsn: process.env.SENTRY_DSN,
 };
